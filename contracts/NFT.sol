772e3cf5396f01c31fd0036803080846323204f7
lalithbd <87491297+lalithbd@users.noreply.github.com>
2024-08-26 17:55:59 +0800

    Add files via upload
    PERC-20

---
+++ contracts/NFT.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract TestNFT is ERC721, ERC721Burnable {
+    constructor()
+        ERC721("SNFT","SWISSNFT")
+    {}
+
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+}
---
+++ contracts/Token.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("SWISSETH","SETH"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
